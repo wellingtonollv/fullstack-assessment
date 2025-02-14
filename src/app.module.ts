332022f90c12fff5b0331ad4ab3cb2596c28b6dc
wellingtonollv <28570773+wellingtonollv@users.noreply.github.com>
2025-02-14 01:46:18 -0300

    ✨ feat: rename getHello to getPing and update AppModule to include PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EmployeeController } from './employee/employee.controller';
-import { DepartmentController } from './department/department.controller';
 import { EmployeeModule } from './employee/employee.module';
 import { DepartmentModule } from './department/department.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [EmployeeModule, DepartmentModule],
-  controllers: [AppController, EmployeeController, DepartmentController],
+  imports: [PrismaModule, EmployeeModule, DepartmentModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
