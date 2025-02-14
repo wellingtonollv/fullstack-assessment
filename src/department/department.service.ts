b683e8848f771847c90194afb901f7370daa968a
wellingtonollv <28570773+wellingtonollv@users.noreply.github.com>
2025-02-14 01:42:34 -0300

    ✨ feat: enhance Department module with CRUD operations and Prisma integration

--- src/department/department.service.ts
+++ src/department/department.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDepartmentDto } from './dto/create-department.dto';
-import { UpdateDepartmentDto } from './dto/update-department.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class DepartmentService {
+  constructor(private prisma: PrismaService) {}
   create(createDepartmentDto: CreateDepartmentDto) {
-    return 'This action adds a new department';
+    return this.prisma.department.create({ data: createDepartmentDto });
   }
 
   findAll() {
-    return `This action returns all department`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} department`;
-  }
-
-  update(id: number, updateDepartmentDto: UpdateDepartmentDto) {
-    return `This action updates a #${id} department`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} department`;
+    return this.prisma.department.findMany({
+      include: {
+        employees: true,
+      },
+    });
   }
 }
--- src/department/dto/create-department.dto.ts
+++ src/department/dto/create-department.dto.ts
@@ -1,1 +1,12 @@
-export class CreateDepartmentDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateDepartmentDto {
+  @ApiProperty({
+    example: 'HR',
+    description: 'New department name',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
