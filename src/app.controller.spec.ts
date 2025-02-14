7702786fdb3bcc044acac6cff633115f6efb3cc9
wellingtonollv <28570773+wellingtonollv@users.noreply.github.com>
2025-02-14 01:46:44 -0300

    ✅ test: update AppController test to reflect method rename and new return value

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it(`should return "I'm Alive!"`, () => {
+      expect(appController.getPing()).toBe("I'm Alive!");
     });
   });
 });
