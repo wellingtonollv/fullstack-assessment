619dddc88ffb82b5ad8b5569360a94241d473ed7
wellingtonollv <28570773+wellingtonollv@users.noreply.github.com>
2025-03-07 02:22:43 -0300

    📦 feat: add @nestjs/event-emitter dependency and integrate EventEmitterModule in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { EmployeeModule } from './employee/employee.module';
 import { DepartmentModule } from './department/department.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [PrismaModule, EmployeeModule, DepartmentModule],
+  imports: [
+    PrismaModule,
+    EmployeeModule,
+    DepartmentModule,
+    EventEmitterModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
