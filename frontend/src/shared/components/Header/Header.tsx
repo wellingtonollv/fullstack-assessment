093dc5b27f754ed6890c7829c3a99fa68d345816
wellingtonollv <28570773+wellingtonollv@users.noreply.github.com>
2025-04-02 01:28:41 -0300

    :art: style: add horizontal padding to Header component for improved layout

--- frontend/src/shared/components/Header/Header.tsx
+++ frontend/src/shared/components/Header/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <header className="z-10 h-header bg-background/95 text-primary-foreground py-4 sticky top-0 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="px-10 z-10 h-header bg-background/95 text-primary-foreground py-4 sticky top-0 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex items-center justify-between">
         <Logo className="text-black dark:text-white" />
 
